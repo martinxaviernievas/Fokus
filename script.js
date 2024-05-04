c06c2771b5460cde838af455f32a54a4ada1c68b
MartinXavierNievas <100978531+martinxaviernievas@users.noreply.github.com>
2024-05-04 15:03:18 -0300

    Finalizado

--- script.js
+++ script.js
@@ -7,6 +7,18 @@ const titulo = document.querySelector('.app__title');
 const botones = document.querySelectorAll('.app__card-button')
 const inputEnfoqueMusica = document.querySelector('#alternar-musica')
 const musica = new Audio ('/sonidos/luna-rise-part-one.mp3')
+const botonInicialPausar = document.querySelector('#start-pause');
+const audioPausa = new Audio('/sonidos/pause.mp3');
+const audioPlay = new Audio('./sonidos/play.wav');
+const audioTiempoFinalizado = new Audio('/sonidos/beep.mp3')
+const textoIniciarPausar = document.querySelector('#start-pause span')
+const iconoIniciarPausar = document.querySelector(".app__card-primary-butto-icon")
+const tiempoEnPantalla = document.querySelector('#timer')
+
+let tiempoTranscurridoEnSegundos = 1500
+let idIntervalo = null
+
+musica.loop = true
 
 inputEnfoqueMusica.addEventListener('change', ()=> {
     if(musica.paused){
@@ -16,28 +28,29 @@ inputEnfoqueMusica.addEventListener('change', ()=> {
     }
 })
 
-
 botonCorto.addEventListener('click', () => {
+    tiempoTranscurridoEnSegundos = 300
     cambiarContexto('descanso-corto' )
     botonCorto.classList.add('active')
 })
 
 botonEnfoque.addEventListener('click', () => {
+   tiempoTranscurridoEnSegundos = 1500
    cambiarContexto('enfoque')
    botonEnfoque.classList.add('active')
 }) 
 
 botonLargo.addEventListener('click', () => {
+    tiempoTranscurridoEnSegundos = 900
     cambiarContexto('descanso-largo' )
     botonLargo.classList.add('active')
 }) 
 
-function cambiarContexto(contexto){
-
+function cambiarContexto(contexto) {
+    mostrarTiempo()
     botones.forEach(function(contexto){
-        contexto.classList.remove('active')
-    })
-
+        contexto.classList.remove('active');
+    });
     html.setAttribute ('data-contexto',contexto) 
     banner.setAttribute('src', `./imagenes/${contexto}.png`)
 
@@ -60,3 +73,43 @@ function cambiarContexto(contexto){
     }
 
 }
+
+const cuentaRegresiva = () => {
+    if(tiempoTranscurridoEnSegundos <= 0){
+        audioTiempoFinalizado.play();
+        alert('tiempo finalizado')
+        reiniciar()
+        return;
+    }
+    tiempoTranscurridoEnSegundos -=1;
+    mostrarTiempo();
+}
+
+
+botonInicialPausar.addEventListener('click', iniciarPausar)
+
+function iniciarPausar(){
+    if(idIntervalo){
+       audioPausa.play();
+       reiniciar();
+       return;
+    }
+    audioPlay.play();
+    idIntervalo = setInterval(cuentaRegresiva,1000)
+    textoIniciarPausar.textContent = "Pausar"
+    iconoIniciarPausar.setAttribute('src','/imagenes/pause.png');
+}
+
+function reiniciar(){
+    clearInterval(idIntervalo)
+    textoIniciarPausar.textContent = "Comenzar"
+    iconoIniciarPausar.setAttribute('src','/imagenes/play_arrow.png');
+    idIntervalo = null
+}
+
+function mostrarTiempo(){
+    const tiempo =  new Date(tiempoTranscurridoEnSegundos * 1000);
+    const tiempoFormateado = tiempo.toLocaleTimeString('es-ES', {minute : '2-digit',second:'2-digit'});
+    tiempoEnPantalla.innerHTML = `${tiempoFormateado}`;
+}
+mostrarTiempo();
